496abf46d30007a3219fdaedac2205a02740cbc0
Jadira2003 <pandashinanathaly@gmail.com>
2026-05-06 16:52:13 -0500

    Primer commit del proyecto GITs

---
+++ src/git/ejeplo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package git;
+
+/**
+ *
+ * @author Admin
+ */
+public class ejeplo {
+      public static void main(String[] args) {
+        System.out.println("Primera version del proyecto INVESTIGACION");
+    }
+}
+
