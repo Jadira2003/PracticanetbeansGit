63fc28c57d8d7f330f12e8fb2eb5caf3d6db3fe5
Jadira2003 <pandashinanathaly@gmail.com>
2026-05-07 15:25:13 -0500

    Revert "solo agrege mi nombre"
    This reverts commit e55e32c1c121b91d17a0ab84c54f2caf27d33402.

--- src/git/ejeplo.java
+++ src/git/ejeplo.java
@@ -10,7 +10,7 @@ package git;
  */
 public class ejeplo {
       public static void main(String[] args) {
-        System.out.println("Cambio realizado desde GitHub para probar Pull jadira");
+        System.out.println("Cambio realizado desde GitHub para probar Pull");
     }
 }
 
